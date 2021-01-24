9a921028b37e726c0ecb13937a408b44bdd23130
Fran Jimenez Zamora <42138637+franJzamora@users.noreply.github.com>
2021-01-24 12:54:32 +0100

    Doing my portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--     <HelloWorld msg="Welcome to Your Vue.js App"/>
+ -->
+   <section class="text-gray-700">
+            <div class="container">
+                <div class="flex-1 mx-auto ">
+                    <div class="grid grid-cols-6 gap-4">
+                       <div class="col-start-1 row-span-2 lg:col-span-1 col-span-full border-4 border-light-blue-500">
+                         <div class="flex flex-wrap h-full p-8 bg-gray-175">
+
+                         </div>
+                       </div>
+                      <div class="lg:col-start-2 col-span-full col-span-full border-4 border-light-blue-500">
+                         <p>Hola</p>
+                       </div>
+                       <div class="lg:col-end-7 col-span-full lg:col-span-1 border-4 border-light-blue-500">
+                         <p>Hola</p>
+                       </div>
+                       <div class="lg:col-start-2 col-span-full lg:col-span-2 border-4 border-light-blue-500">
+                         <p>Hola</p>
+                       </div>
+                       <div class="row-span-2 lg:col-end-7 col-span-full lg:col-span-3 border-4 border-light-blue-500">
+                         <p>Hola</p>
+                       </div>
+                    </div>
+                </div>
+            </div>
+   
+        </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+   /*  HelloWorld, */
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
